bcfbab610a90a6b88135ef5946505c5fa3a330e2
Ava <55948841+AvaLee121@users.noreply.github.com>
2024-01-09 22:46:03 +0000

    update

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+const MenuPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MenuPage
---
+++ src/components/CartIcon.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+
+const CartIcon = () => {
+  return (
+    <div>
+
+      <Link href="/cart" className='flex items-center gap-4'>
+        <div className='relative w-8 h-8'>
+            <Image src="/cart.png" alt="" fill/>
+        </div>
+        <span>Cart (3)</span>
+      </Link>
+      
+    </div>
+  )
+}
+
+export default CartIcon
