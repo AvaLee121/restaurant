a4d2c673b90ee390d0750be43f045e9657596f95
Ava <55948841+AvaLee121@users.noreply.github.com>
2024-01-12 15:14:39 +0000

    update

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -7,7 +7,7 @@ const CategoryPage = () => {
   return (
     <div className="flex flex-wrap md:flex-row text-red-500">
       {pizzas.map((product) => (
-        <div className="w-full p-4 h-[60vh] border-b-2 border-r-2 border-red-500 sm:w-1/2 md:w-1/3  group">
+        <div className="w-full p-4 h-[60vh] border-b-2 border-r-2 border-red-500 sm:w-1/2 md:w-1/3  group even:bg-fuchsia-50">
           <Link href={`/product/${product.id}`} key={product.id} className="">
           {product.img && ( 
             <div  className="relative h-[80%]">       
@@ -20,9 +20,9 @@ const CategoryPage = () => {
             </div>   
           )}
           </Link>
-          <div className="flex flex-row py-4 px-1 items-center justify-between text-red-500">
-            <p className="font-bold uppercase text-2xl sm:text-lg ">{product.title}</p>
-            <span className="group-hover:hidden font-bold text-xl sm:text-lg">${product.price}
+          <div className="flex flex-row py-4 px-1 items-center justify-between text-red-500 xl:p-8">
+            <p className="font-bold uppercase text-2xl sm:text-lg xl:text-2xl ">{product.title}</p>
+            <span className="group-hover:hidden font-bold text-xl sm:text-lg xl:text-2xl">${product.price}
             </span>
             <button className="hidden group-hover:block bg-red-500 text-white sm:text-sm p-1 rounded-sm">Add to Cart</button>
           </div>
