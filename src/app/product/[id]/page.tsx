a51641f3f78932eb49b6b294e3bc609f6a367e9c
Ava <55948841+AvaLee121@users.noreply.github.com>
2024-01-12 15:25:51 +0000

    update

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
+import {pizzas} from '@/data'
 
 const SingleProduct = () => {
   return (
-    <div>
-      SingleProduct
+    <div className=''>
+
+      {pizzas.map(item=>(
+      <div className=''>
+        {/* img container */}
+        {item.img && <div className=''>
+          <Image src={''} alt={''}>{item.img}</Image>
+        </div>}
+        {/* text container */}
+        <div className=''>
+          <h1 className=''>{item.title}</h1>
+          <p className=''>{item.price}</p>
+          {item.options && item.options.map(option=>(
+            <span className=''>{option}</span>
+          ))}
+          <button className=''>Add to Cart</button>
+        </div>
+
+      </div>
+      ))}
+
     </div>
   )
 }
